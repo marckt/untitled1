6ae10748dd324bf8d12ecbdbf6f711258d11bdd6
marckt <marck.for@yandex.ru>
2018-09-05 17:23:59 +0700

    /zxcxczc

--- src/ex2_12.java
+++ src/ex2_12.java
@@ -1,10 +1,12 @@
 public class ex2_12 {
     public static void main(String[] args) {
-        System.out.println(countCharsInOrder();
+        System.out.println(countCharsInOrder("xzczxcabab",'a','b'));
     }
     public static int countCharsInOrder(String str, char ch1, char ch2){
-        //put your code here
-        return 0;
+        String concat = Character.toString(ch1)+Character.toString(ch2);
+        String newString = str.replaceAll(concat,"");
+
+        return (str.length() - newString.length())/2;
     }
 
 }
