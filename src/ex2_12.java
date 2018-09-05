afa4e9d0eda7c74a4dea4597f7c63a5124e4d498
marckt <marck.for@yandex.ru>
2018-09-05 17:15:22 +0700

    /zxcxczc

---
+++ src/ex2_12.java
@@ -0,0 +1,10 @@
+public class ex2_12 {
+    public static void main(String[] args) {
+        System.out.println(countCharsInOrder();
+    }
+    public static int countCharsInOrder(String str, char ch1, char ch2){
+        //put your code here
+        return 0;
+    }
+
+}
